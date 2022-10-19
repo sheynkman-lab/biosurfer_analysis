451450535ec965efa35f55068131ece3115e6049
Mayank Murali <mayankmurali12@gmail.com>
2022-10-19 06:43:29 -0400

    Ran analysis on gencode_toy, gencode_v41 and wtc11

--- main.sh
+++ main.sh
@@ -2,7 +2,6 @@
 
 # Step 1 - create the conda environment for biosurfer
 
-# bash ./scripts/create_conda_env.sh
 conda create --name biosurfer-install --channel conda-forge python=3 pip 
 
 # Then activate the environment:
@@ -25,17 +24,33 @@ bash ./scripts/download_gencode_toy.sh
 # Step 4 - Create toy dataset on biosurfer
 cd biosurfer # Have to cd in to biosurfer directory to run biosurfer commands
 
-biosurfer load_db \
- --source=GENCODE \
- --gtf ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.gtf \
- --tx_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.transcripts.fa \
- --tl_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.translations.fa \
- -d gencode_toy
+    # gencode_toy
+    biosurfer load_db \
+    --source=GENCODE \
+    --gtf ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.gtf \
+    --tx_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.transcripts.fa \
+    --tl_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.translations.fa \
+    -d gencode_toy
+
+    # gencode_v41
+    biosurfer load_db --source=GENCODE --gtf ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d gencode_v41
+
+    # wtc11
+    biosurfer load_db --source=GENCODE --gtf ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d wtc11_db
+    biosurfer load_db --source=PacBio --gtf ../A_wtc11/biosurfer_wtc11_data/wtc11_with_cds.gtf --tx_fasta ../A_wtc11/biosurfer_wtc11_data/wtc11_corrected.fasta --tl_fasta ../A_wtc11/biosurfer_wtc11_data/wtc11_orf_refined.fasta --sqanti ../A_wtc11/biosurfer_wtc11_data/wtc11_classification.txt -d wtc11_db
 
 # Step 5 - Run biosurfer hybrid alignment on the toy dataset
-mkdir ../B_hybrid_aln_results_toy
-biosurfer hybrid_alignment -d gencode_toy -o ../B_hybrid_aln_results_toy --gencode
+    # gencode_toy
+    mkdir ../B_hybrid_aln_results_toy
+    biosurfer hybrid_alignment -d gencode_toy -o ../B_hybrid_aln_results_toy --gencode
 
+    # gencode_v41
+    mkdir ../B_hybrid_aln_gencode_v41
+    biosurfer hybrid_alignment -d gencode_v41 -o ../B_hybrid_aln_gencode_v41 --gencode
+
+    # wtc11
+    mkdir ../B_hybrid_aln_wtc11
+    biosurfer hybrid_alignment -d wtc11 -o ../B_hybrid_aln_gencode_v41 
 
 ## Steps from here until step 10 will be based on Jupyter notebook (ipynb) for plotting
 
@@ -52,6 +67,8 @@ pip install xlsxwriter openpyxl plotly
 
     #Else on ipynb through website (pip install notebook)
     jupyter notebook ./scripts/genome_wide_summary.ipynb
+
+    # Have a third option to run .py to spit out 
 
 
 # Step 7 - N-termini summary 
