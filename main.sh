578ad12a8414e62277cd05ea2920c5568e791958
Jamie Saquing <xht7mt@virginia.edu>
2022-10-19 09:48:53 -0400

    add other download scripts to main.sh

--- main.sh
+++ main.sh
@@ -18,44 +18,44 @@ conda install --channel conda-forge graph-tool
     # Run setup 
     pip install --editable biosurfer
 
-# Step 3 - download GENCODE toy data
-bash ./scripts/download_gencode_toy.sh 
+# Step 3 - download input data
+for source in gencode_toy gencode_v41 wtc11
+do
+    bash "./scripts/download_$source.sh"
+done
 
 # Step 4 - Create toy dataset on biosurfer
-cd biosurfer # Have to cd in to biosurfer directory to run biosurfer commands
-
     # gencode_toy
     biosurfer load_db \
     --source=GENCODE \
-    --gtf ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.gtf \
-    --tx_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.transcripts.fa \
-    --tl_fasta ../A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.translations.fa \
+    --gtf A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.gtf \
+    --tx_fasta A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.transcripts.fa \
+    --tl_fasta A_gencode_toy/biosurfer_gencode_toy_data/gencode.v38.toy.translations.fa \
     -d gencode_toy
 
     # gencode_v41
-    biosurfer load_db --source=GENCODE --gtf ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d gencode_v41
+    biosurfer load_db --source=GENCODE --gtf A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d gencode_v41
 
     # wtc11
-    biosurfer load_db --source=GENCODE --gtf ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta ../A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d wtc11_db
-    biosurfer load_db --source=PacBio --gtf ../A_wtc11/biosurfer_wtc11_data/wtc11_with_cds.gtf --tx_fasta ../A_wtc11/biosurfer_wtc11_data/wtc11_corrected.fasta --tl_fasta ../A_wtc11/biosurfer_wtc11_data/wtc11_orf_refined.fasta --sqanti ../A_wtc11/biosurfer_wtc11_data/wtc11_classification.txt -d wtc11_db
+    biosurfer load_db --source=GENCODE --gtf A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.annotation.gtf --tx_fasta A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_transcripts.fa --tl_fasta A_gencode_v41/biosurfer_gencode_v41_data/gencode.v41.pc_translations.fa -d wtc11_db
+    biosurfer load_db --source=PacBio --gtf A_wtc11/biosurfer_wtc11_data/wtc11_with_cds.gtf --tx_fasta A_wtc11/biosurfer_wtc11_data/wtc11_corrected.fasta --tl_fasta A_wtc11/biosurfer_wtc11_data/wtc11_orf_refined.fasta --sqanti A_wtc11/biosurfer_wtc11_data/wtc11_classification.txt -d wtc11_db
 
 # Step 5 - Run biosurfer hybrid alignment on the toy dataset
     # gencode_toy
-    mkdir ../B_hybrid_aln_results_toy
-    biosurfer hybrid_alignment -d gencode_toy -o ../B_hybrid_aln_results_toy --gencode
+    mkdir B_hybrid_aln_results_toy
+    biosurfer hybrid_alignment -d gencode_toy -o B_hybrid_aln_results_toy --gencode
 
     # gencode_v41
-    mkdir ../B_hybrid_aln_gencode_v41
-    biosurfer hybrid_alignment -d gencode_v41 -o ../B_hybrid_aln_gencode_v41 --gencode
+    mkdir B_hybrid_aln_gencode_v41
+    biosurfer hybrid_alignment -d gencode_v41 -o B_hybrid_aln_gencode_v41 --gencode
 
     # wtc11
-    mkdir ../B_hybrid_aln_wtc11
-    biosurfer hybrid_alignment -d wtc11 -o ../B_hybrid_aln_gencode_v41 
+    mkdir B_hybrid_aln_wtc11
+    biosurfer hybrid_alignment -d wtc11 -o B_hybrid_aln_gencode_v41 
 
 ## Steps from here until step 10 will be based on Jupyter notebook (ipynb) for plotting
 
 #Step 6 - Post-process hybrid alignment output table / genome wide analysis
-cd .. # cd out of biosurfer directory
 mkdir C_toy_plots
 
 #Install required libraries 
