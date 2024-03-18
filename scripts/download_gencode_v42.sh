84ff94431322eb362113d9979d2041c6f521c32a
Mayank Murali <mmurali@wmdb9-047.broadinstitute.org>
2024-03-18 12:51:51 -0400

    Updated README and dowload links

--- scripts/download_gencode_v42.sh
+++ scripts/download_gencode_v42.sh
@@ -12,6 +12,6 @@ cd data
 mkdir A_gencode_v42
 cd A_gencode_v42
 
-wget https://zenodo.org/record/7297008/files/biosurfer_gencode_v42_data.zip
+wget https://zenodo.org/records/10822882/files/biosurfer_gencode_v42_data.zip
 unzip biosurfer_gencode_v42_data.zip 
 rm -rf __MACOSX biosurfer_gencode_v42_data.zip
